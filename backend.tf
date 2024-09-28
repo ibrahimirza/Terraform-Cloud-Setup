f834f844e1908b42228ed8e752b602bea1638d07
Mohd Ibrahim Mirza <73487125+ibrahimirza@users.noreply.github.com>
2024-09-28 23:12:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ibrahimterra"
-    key    = "ibrahim/terraform.tfstate"
+    bucket = "yourBucketName"
+    key    = "yourKey"
     region = "us-east-1"
   }
 }
